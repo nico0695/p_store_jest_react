8807c85a7ab848866fa2983eca7a7017b068e03b
unknown <nico0695@gmail.com>
2022-02-22 22:16:01 -0300

    init

---
+++ src/__test__/actions/actions.test.js
@@ -0,0 +1,15 @@
+import actions from '../../actions'
+import ProductMock from '../../__mocks__/ProductMock'
+
+describe('AActions', () => {
+  test('addToCart Action', () => {
+    const payload = ProductMock;
+    const expected = {
+      type: "ADD_TO_CART",
+      payload,
+    }
+
+    expect(actions.addToCart(payload)).toEqual(expected)
+  })
+
+})
---
+++ src/__test__/components/Header.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import ProviderMock from '../../__mocks__/ProviderMock';
+import Header from '../../components/Header';
+
+import { create } from 'react-test-renderer';
+
+describe('<Header />', () => {
+  test('Render del componente Header', () => {
+    const header = shallow(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>
+    );
+
+    expect(header.length).toEqual(1);
+  });
+
+  test('Render del titulo', () => {
+    const header = mount(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>
+    );
+
+    expect(header.find('.Header-title').text()).toEqual('Platzi Store');
+  });
+});
+
+describe('Header Snapshot', () => {
+  test('Comprobar el Snapshot de Header', () => {
+    const header = create(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>
+    );
+    expect(header.toJSON()).toMatchSnapshot();
+  });
+});
